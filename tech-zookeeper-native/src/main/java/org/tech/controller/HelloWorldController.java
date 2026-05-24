646184e420aaa651a554621651c959f42365a058
yushen1202 <yushen1202@hotmail.com>
2026-05-24 18:13:05 +0800

    first commit

---
+++ tech-zookeeper-native/src/main/java/org/tech/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package org.tech.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/helloWorld")
+    public String helloWorld() {
+        return "hello world";
+    }
+}
